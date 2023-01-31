f45274974fb492e2a7186a6f5e7a16001f3c11be
Anshumaan Tyagi <anshumaantyagi0@gmail.com>
2023-02-01 05:09:15 +0800

    First commit

---
+++ submission_example.sh
@@ -0,0 +1,2 @@
+# please replace the folder path with your own path
+./azcopy copy "/Users/anshumaantyagi/Desktop/Personal/Projects/SAP/anshumaantyagi_100902" "https://hiringstorageacc.blob.core.windows.net/hiring-artefacts/submissions/?sp=w&st=2022-01-10T03:53:23Z&se=2023-01-10T11:53:23Z&spr=https&sv=2020-08-04&sr=d&sig=nlQekuQ4EKdNuZ9%2F8h5NjVLJPUO%2FR6zB1SnW1xcdglc%3D&sdd=1" --recursive=true
